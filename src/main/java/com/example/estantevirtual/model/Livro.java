8a5b54f1841306ce54cc9a7eafbf56d71989964f
Diego Cordeiro <dcordeiro.eng@gmail.com>
2022-07-09 12:05:31 -0300

    Inclusão de validações

--- src/main/java/com/example/estantevirtual/model/Livro.java
+++ src/main/java/com/example/estantevirtual/model/Livro.java
@@ -8,7 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Getter
@@ -18,10 +19,11 @@ public class Livro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message = "Título é obrigatório")
     @Length(min = 3, max = 100, message = "O título do livro deve ter entre {min} e {max} caracteres")
     private String titulo;
-    @NotBlank(message = "Autor é obrigatório")
     @Length(min = 3, max = 35, message = "O nome do autor deve ter entre {min} e {max} caracteres")
     private String autor;
+    @NotNull(message = "Necessário informar o número de páginas")
+    @Min(value = 1, message = "Número de páginas deve ser maior que 0")
+    private Integer paginas;
 }
